280ff8746301ec1f98860fabdd8f19a5982572b7
Patcharapong10 <patcharapong20new@gmail.com>
2021-04-13 21:58:06 +0700

    cc

--- mongo.py
+++ mongo.py
@@ -13,4 +13,4 @@ def index():
     return render_template('index.html', emp_list = emp_list)
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1',port = 80)
+    app.run(host='0.0.0.0',port = 80)
